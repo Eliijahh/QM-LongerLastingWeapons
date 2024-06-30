86ff5b783c9fa8aff2f488f545c552f378b84c25
Paolo Caponeri <paolo.caponeri@gmail.com>
2024-06-30 15:38:02 +0200

    Initial Setup

---
+++ IncreaseWeaponsDurability/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncreaseWeaponsDurability
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class IncreaseWeaponsDurability : BaseUnityPlugin
+    {
+        private const string modGUID = "Eliijahh.IncreaseWeaponsDurability";
+        private const string modNmae = "Increase Weapons Durability";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static IncreaseWeaponsDurability Instance;
+
+        void Awake()
+        {
+            if(Instance == null) 
+            {
+                Instance = this;
+            }
+        }
+    }
+}
